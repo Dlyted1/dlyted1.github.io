0e629f6a5da29c158a1f6cb0c501e8cc1292c1cf
Dlyted1 <mookathy3@gmail.com>
2021-05-20 13:02:32 -0400

    commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,8 +1,10 @@
 //determine day of week//
-let thedate = new Date();
+    let thedate = new Date();
 // console.Log(thedate.getDay());
 
 // if it's Friday -day 5 or true then turn the aside display property to block
-if (thedate.getDay() == 4) {
-    document.querySelector('#banner').getElementsByClassName.display = 'block';
+    if (thedate.getDay() == 4) {
+       document.querySelector('#banner').getElementsByClassName.display = 'block';
 }
+
+// now do the above in one statement by using terinary statement
