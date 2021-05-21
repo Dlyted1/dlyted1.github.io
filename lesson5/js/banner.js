718ae90052fa0ba7233c40edb850430fec191a9b
Dlyted1 <mookathy3@gmail.com>
2021-05-21 15:46:52 -0400

    commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -3,7 +3,7 @@
 // console.Log(thedate.getDay());
 
 // if it's Friday -day 5 or true then turn the aside display property to block
-    if (thedate.getDay() == 4) {
+    if (thedate.getDay() == 5) {
        document.querySelector('#banner').getElementsByClassName.display = 'block';
 }
 
